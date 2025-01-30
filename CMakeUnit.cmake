1bf92ca76b2d037d1d54de328a30f914e6c89b4b
Maciej Stankiewicz <tawezik@gmail.com>
2025-01-30 14:46:39 +0100

    feat: simplify add_cmake_test api

--- CMakeUnit.cmake
+++ CMakeUnit.cmake
@@ -1,42 +1,45 @@
-function(set_cmake_test_name variable test)
-    cmake_path(GET CMAKE_CURRENT_SOURCE_DIR STEM suite)
-    string(REPLACE "/" "::" test "${test}")
-    set(${variable} "${suite}::${test}" PARENT_SCOPE)
-endfunction()
+function(add_cmake_test file name)
+    set(options SKIP WILL_FAIL)
+    set(oneValueArgs "")
+    set(multiValueArgs "")
+    cmake_parse_arguments(test "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
+    get_filename_component(path "${file}" ABSOLUTE)
+    if (IS_DIRECTORY "${path}")
+        cmake_path(APPEND file "CMakeLists.txt")
+    endif ()
+    get_filename_component(path "${file}" ABSOLUTE)
+    if (NOT EXISTS "${path}")
+        message(FATAL_ERROR "${file} not found")
+    elseif (IS_DIRECTORY "${path}")
+        message(FATAL_ERROR "${file} is not a file")
+    endif ()
 
-function(add_cmake_test test)
-    cmake_parse_arguments(arg "WILL_FAIL" "" "" ${ARGN})
+    if (${test_SKIP})
+        add_test(
+            NAME "${name}"
+            COMMAND ${CMAKE_COMMAND} -E echo "SKIP THIS TEST"
+        )
+        set_property(TEST "${name}" PROPERTY SKIP_REGULAR_EXPRESSION "SKIP THIS TEST")
+        return()
+    endif ()
 
     configure_file(
-        "${CMAKE_CURRENT_SOURCE_DIR}/${test}.cmake"
-        "${CMAKE_CURRENT_BINARY_DIR}/${test}.cmake"
+        "${CMAKE_CURRENT_SOURCE_DIR}/${file}"
+        "${CMAKE_CURRENT_BINARY_DIR}/${file}"
         @ONLY
     )
 
-    set_cmake_test_name(testName "${test}")
-
     add_test(
-        NAME "${testName}"
+        NAME "${name}"
         COMMAND ${CMAKE_COMMAND}
-        ${arg_UNPARSED_ARGUMENTS}
+        ${test_UNPARSED_ARGUMENTS}
         "-DCMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}"
-        -P "${CMAKE_CURRENT_BINARY_DIR}/${test}.cmake"
+        -P "${CMAKE_CURRENT_BINARY_DIR}/${file}"
     )
-    if (${arg_WILL_FAIL})
-        set_property(TEST "${testName}" PROPERTY WILL_FAIL true)
+    if (${test_WILL_FAIL})
+        set_property(TEST "${name}" PROPERTY WILL_FAIL true)
     endif ()
-endfunction()
-
-
-function(xadd_cmake_test test)
-    set_cmake_test_name(testName "${test}")
-
-    add_test(
-        NAME "${testName}"
-        COMMAND ${CMAKE_COMMAND} -E echo "SKIP THIS TEST"
-    )
-    set_property(TEST "${testName}" PROPERTY SKIP_REGULAR_EXPRESSION "SKIP THIS TEST")
 endfunction()
 
 
@@ -51,11 +54,7 @@ endmacro()
 
 # {{{ Assertions
 macro(FAIL)
-    if ("${ARGN}" STREQUAL "")
-        message(SEND_ERROR "Failed")
-    else ()
-        message(SEND_ERROR "${ARGN}")
-    endif ()
+    message(SEND_ERROR "${ARGN}")
 endmacro()
 
 
@@ -115,5 +114,50 @@ macro(EXPECT_NOT_MATCH value regexp)
         message(SEND_ERROR "'${value}' is expected not to match '${regexp}'")
     endif ()
 endmacro()
+
+
+#See also the Floating-Point Comparison assertions to compare floating-point numbers and avoid problems caused by rounding.
+#EXPECT_EQ
+#
+#EXPECT_EQ(val1,val2)
+#ASSERT_EQ(val1,val2)
+#
+#Verifies that val1==val2.
+#
+#Does pointer equality on pointers. If used on two C strings, it tests if they are in the same memory location, not if they have the same value. Use EXPECT_STREQ to compare C strings (e.g. const char*) by value.
+#
+#When comparing a pointer to NULL, use EXPECT_EQ(ptr, nullptr) instead of EXPECT_EQ(ptr, NULL).
+#EXPECT_NE
+#
+#EXPECT_NE(val1,val2)
+#ASSERT_NE(val1,val2)
+#
+#Verifies that val1!=val2.
+#
+#Does pointer equality on pointers. If used on two C strings, it tests if they are in different memory locations, not if they have different values. Use EXPECT_STRNE to compare C strings (e.g. const char*) by value.
+#
+#When comparing a pointer to NULL, use EXPECT_NE(ptr, nullptr) instead of EXPECT_NE(ptr, NULL).
+#EXPECT_LT
+#
+#EXPECT_LT(val1,val2)
+#ASSERT_LT(val1,val2)
+#
+#Verifies that val1<val2.
+#EXPECT_LE
+#
+#EXPECT_LE(val1,val2)
+#ASSERT_LE(val1,val2)
+#
+#Verifies that val1<=val2.
+#EXPECT_GT
+#
+#EXPECT_GT(val1,val2)
+#ASSERT_GT(val1,val2)
+#
+#Verifies that val1>val2.
+#EXPECT_GE
+#
+#EXPECT_GE(val1,val2)
+#ASSERT_GE(val1,val2)
 
 # }}} Assertions
