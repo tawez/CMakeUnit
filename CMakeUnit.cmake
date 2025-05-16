ac37e4d5e47792d351bd53b4e9e28f1120def935
Maciej Stankiewicz <tawezik@gmail.com>
2025-05-16 14:29:54 +0200

    feat: add checks

--- CMakeUnit.cmake
+++ CMakeUnit.cmake
@@ -23,6 +23,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+
+cmake_policy(SET CMP0057 NEW)
+
+
 # {{{ Core functions
 function(add_cmake_test name path)
     set(options SKIP WILL_FAIL)
@@ -76,3 +80,120 @@ macro(add_cmakeunit_target name)
     )
 endmacro()
 # }}} Core functions
+
+
+# {{{ CMakeUnit internal helpers
+function(CMakeUnit_check_relation relation)
+    set(validRelation EQ NE LT LE GT GE)
+    if (NOT relation IN_LIST validRelation)
+        FATAL("Invalid relation '${relation}'")
+    endif ()
+endfunction()
+# }}} CMakeUnit internal helpers
+
+
+# {{{ Assertions
+macro(FATAL)
+    message(FATAL_ERROR "${ARGN}")
+endmacro()
+
+macro(FAIL)
+    message(SEND_ERROR "${ARGN}")
+endmacro()
+
+macro(EXPECT_TRUE value)
+    block(SCOPE_FOR POLICIES)
+        cmake_policy(SET CMP0012 NEW)
+        if (NOT ${value})
+            FAIL("'${value}' is expected to be 'true'")
+        endif ()
+    endblock()
+endmacro()
+
+macro(EXPECT_FALSE value)
+    block(SCOPE_FOR POLICIES)
+        cmake_policy(SET CMP0012 NEW)
+        if (${value})
+            FAIL("'${value}' is expected to be 'false'")
+        endif ()
+    endblock()
+endmacro()
+
+macro(EXPECT_DEFINED variable)
+    if (NOT DEFINED ${variable})
+        FAIL("'${variable}' is expected to be defined")
+    endif ()
+endmacro()
+
+macro(EXPECT_UNDEFINED variable)
+    if (DEFINED ${variable})
+        FAIL("'${variable}' is expected not to be defined")
+    endif ()
+endmacro()
+
+macro(EXPECT_STREQ value expected)
+    if (NOT "${value}" STREQUAL "${expected}")
+        FAIL("'${value}' is expected to equal to '${expected}'")
+    endif ()
+endmacro()
+
+macro(EXPECT_NOT_STREQ value expected)
+    if ("${value}" STREQUAL "${expected}")
+        FAIL("'${value}' is expected not to equal to '${expected}'")
+    endif ()
+endmacro()
+
+macro(EXPECT_MATCH value pattern)
+    if (NOT "${value}" MATCHES "${pattern}")
+        FAIL("'${value}' is expected to match '${pattern}'")
+    endif ()
+endmacro()
+
+macro(EXPECT_NOT_MATCH value pattern)
+    if ("${value}" MATCHES "${pattern}")
+        FAIL("'${value}' is expected not to match '${pattern}'")
+    endif ()
+endmacro()
+
+function(EXPECT_EQ value expected)
+    if (NOT value EQUAL expected)
+        FAIL("'${value}' is expected to equal to '${expected}'")
+    endif ()
+endfunction()
+
+function(EXPECT_NE value expected)
+    if (value EQUAL expected)
+        FAIL("'${value}' is expected not to equal to '${expected}'")
+    endif ()
+endfunction()
+
+function(EXPECT_LT value expected)
+    if (NOT value LESS expected)
+        FAIL("'${value}' is expected to be lower than '${expected}'")
+    endif ()
+endfunction()
+
+function(EXPECT_LE value expected)
+    if (NOT value LESS_EQUAL expected)
+        FAIL("'${value}' is expected to be lower than or equal to '${expected}'")
+    endif ()
+endfunction()
+
+function(EXPECT_GT value expected)
+    if (NOT value GREATER expected)
+        FAIL("'${value}' is expected to be greater than '${expected}'")
+    endif ()
+endfunction()
+
+function(EXPECT_GE value expected)
+    if (NOT value GREATER_EQUAL expected)
+        FAIL("'${value}' is expected to be greater than or equal to '${expected}'")
+    endif ()
+endfunction()
+
+function(EXPECT_LIST_LENGTH variable relation expected)
+    CMakeUnit_check_relation(${relation})
+    list(LENGTH ${variable} actualLength)
+    cmake_language(CALL "EXPECT_${relation}" ${actualLength} ${expected})
+endfunction()
+# }}} Assertions
